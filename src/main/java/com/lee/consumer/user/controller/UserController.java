b4dcc99b6d478a8abd2194146cdc6004606c3c0d
litong <835881846@QQ.com>
2019-09-04 17:46:33 +0800

    update userController userList

--- src/main/java/com/lee/consumer/user/controller/UserController.java
+++ src/main/java/com/lee/consumer/user/controller/UserController.java
@@ -14,7 +14,8 @@ public class UserController {
     public List<User> userList(){
         List<User> listUser = new ArrayList<>();
         listUser.add(new User(1,"jay",30));
-        listUser.add(new User(1,"Lin",34));
+        listUser.add(new User(2,"Lin",34));
+        listUser.add(new User(3,"Ling",36));
         return listUser;
     }
 }
